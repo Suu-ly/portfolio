ede2ae1ce71db0f1210506bdb6fdc2468e0e0632
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-06-08 21:15:33 +0800

    Scroll cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ReactLenis from "lenis/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ReactLenis root options={{ lerp: 0.15 }} />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,128 @@
-import Image from "next/image";
+"use client";
+import { motion, MotionValue, useScroll, useTransform } from "motion/react";
+import { ComponentProps, CSSProperties, useMemo, useRef } from "react";
+
+const MAGNITUDES = [0.724, 0.261, 0.957, 0.385, 0.773, 0.183];
+
+const Card = ({
+  progress,
+  index,
+  total,
+  ...rest
+}: {
+  progress: MotionValue<number>;
+  index: number;
+  total: number;
+} & ComponentProps<typeof motion.div>) => {
+  const magnitude = MAGNITUDES[index % 6];
+  const dir = Math.floor(magnitude * 100) % 2 === 0 ? 1 : -1;
+  const dirIndex = index % 2 === 0 ? 1 : -1;
+  const rotate = useTransform(
+    progress,
+    [
+      (index - 2) * (1 / total),
+      (index - 1) * (1 / total),
+      (index + 1) * (1 / total),
+      (index + 2) * (1 / total),
+    ],
+    [
+      `${magnitude * 9 * -dir}deg`,
+      `${magnitude * 3 * -dir}deg`,
+      `${magnitude * 3 * dir}deg`,
+      `${magnitude * 9 * dir}deg`,
+    ],
+    { clamp: false }
+  );
+  const x = useTransform(
+    progress,
+    [(index - 1) * (1 / total), (index + 1) * (1 / total)],
+    [`${(1 / magnitude) * 12}%`, `${-(1 / magnitude) * 12}%`],
+    { clamp: false }
+  );
+  const y = useTransform(
+    progress,
+    [(index - 1) * (1 / total), (index + 1) * (1 / total)],
+    [
+      `${(1 / magnitude) * 2 * dir * dirIndex}%`,
+      `${(1 / magnitude) * 2 * -dir * dirIndex}%`,
+    ],
+    { clamp: false }
+  );
+
+  return (
+    <motion.div
+      {...rest}
+      className="w-[calc(100vw-2rem)] max-w-4xl h-160 rounded-lg bg-red-400 border-white border-8 shrink-0"
+      style={{ rotate, x: index % 2 === 0 ? x : undefined, y }}
+    />
+  );
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const regionRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: regionRef,
+    offset: ["start end", "end start"],
+  });
+  const array = useMemo(() => Array.from({ length: 4 }), []);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const x = useTransform(
+    scrollYProgress,
+    [0, 1 / (1 + array.length * 0.5), 1],
+    [
+      "20%",
+      `-${50 - Math.min(9, array.length) * 5}%`,
+      `-${200 - (Math.min(9, array.length) - 1) * 10}%`,
+    ],
+    { clamp: false }
+  );
+
+  const mappedProgress = useTransform(
+    scrollYProgress,
+    [
+      1 / (1 + array.length * 0.5),
+      (array.length * 0.5) / (1 + array.length * 0.5),
+    ],
+    [0, 1],
+    {
+      clamp: false,
+    }
+  );
+
+  return (
+    <main>
+      <div className="h-screen w-screen flex items-center justify-center">
+        Welcome
+      </div>
+      <div
+        ref={regionRef}
+        className="h-(--elements)"
+        style={
+          { "--elements": `${100 + array.length * 50}vh` } as CSSProperties
+        }
+      >
+        <div className="sticky top-0 h-screen overflow-hidden">
+          <h1 className="text-4xl font-medium absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Works
+          </h1>
+          <motion.div
+            className="relative h-full flex gap-12 left-full min-w-fit items-center py-32 px-16 before:absolute before:left-80 before:h-20 before:w-[200%] before:bg-background"
+            style={{ x }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {array.map((_, index) => (
+              <Card
+                key={index}
+                progress={mappedProgress}
+                index={index}
+                total={array.length}
+              />
+            ))}
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="h-screen w-screen flex items-center justify-center">
+        Contact me
+      </div>
+    </main>
   );
 }
