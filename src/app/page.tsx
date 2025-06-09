7bad9f6f9e21773bd9f359cdaf8cca14499ad964
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-06-09 22:28:13 +0800

    Card hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,37 @@
 "use client";
-import { motion, MotionValue, useScroll, useTransform } from "motion/react";
-import { ComponentProps, CSSProperties, useMemo, useRef } from "react";
+import { cn } from "@/lib/utils";
+import {
+  motion,
+  MotionValue,
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "motion/react";
+import {
+  ComponentProps,
+  CSSProperties,
+  PointerEvent,
+  ReactNode,
+  useMemo,
+  useRef,
+} from "react";
 
 const MAGNITUDES = [0.724, 0.261, 0.957, 0.385, 0.773, 0.183];
+const ROTATE_MAGNITUDE = 0.05;
 
 const Card = ({
   progress,
   index,
   total,
+  children,
+  className,
   ...rest
 }: {
   progress: MotionValue<number>;
   index: number;
   total: number;
-} & ComponentProps<typeof motion.div>) => {
+} & ComponentProps<typeof motion.div> & { children?: ReactNode }) => {
   const magnitude = MAGNITUDES[index % 6];
   const mapRange = [
     (index - 2) * (1 / total),
@@ -52,12 +70,71 @@ const Card = ({
     { clamp: false }
   );
 
+  const rawX = useMotionValue(0);
+  const rotateX = useSpring(rawX);
+  const shadowX = useTransform(() => rotateX.get() * 3);
+  const rawY = useMotionValue(0);
+  const rotateY = useSpring(rawY);
+  const shadowY = useTransform(() => rotateY.get() * 3);
+  const rawGradientX = useMotionValue(50);
+  const gradientX = useSpring(rawGradientX);
+  const rawGradientY = useMotionValue(50);
+  const gradientY = useSpring(rawGradientY);
+  const background = useTransform(
+    () =>
+      `radial-gradient(farthest-corner circle at ${gradientX.get()}% ${gradientY.get()}%, rgba(255,255,255,0.8) 10%, rgba(255,255,255,0.65) 20%, rgba(255,255,255,0) 90%)`
+  );
+
+  const onPointerMove = (event: PointerEvent<HTMLDivElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const percentageX = ((event.clientX - rect.left) / rect.width) * 100;
+    const percentageY = ((event.clientY - rect.top) / rect.height) * 100;
+    const offsetX = percentageX - 50;
+    const offsetY = percentageY - 50;
+
+    rawY.set(-offsetX * ROTATE_MAGNITUDE);
+    rawX.set(offsetY * ROTATE_MAGNITUDE);
+    gradientX.set(50 + percentageX / 4 - 12.5);
+    gradientY.set(50 + percentageY / 3 - 16.67);
+  };
+
+  const onPointerLeave = () => {
+    rawY.set(0);
+    rawX.set(0);
+    gradientX.set(50);
+    gradientY.set(50);
+  };
+
   return (
     <motion.div
-      {...rest}
-      className="w-[calc(100vw-2rem)] max-w-4xl h-160 rounded-lg bg-red-400 border-white border-8 shrink-0"
-      style={{ rotate, x: index % 2 === 0 ? x : undefined, y }}
-    />
+      style={{ rotateZ, x: index % 2 === 0 ? x : undefined, y }}
+      className="perspective-midrange group isolate relative"
+    >
+      <motion.div
+        {...rest}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+        whileHover={{ scale: 1.05 }}
+        transition={{ duration: 0.2 }}
+        className={cn(
+          "w-[calc(100vw-8rem)] relative min-w-72 max-w-4xl h-160 rounded-2xl bg-slate-800 border-slate-200 border-2 shrink-0 will-change-transform",
+          className
+        )}
+        style={{ rotateX, rotateY }}
+      >
+        {children}
+        <motion.div
+          role="presentation"
+          className="absolute inset-0 mix-blend-overlay duration-500 group-hover:opacity-20 opacity-0 transition-opacity will-change-background rounded-2xl pointer-events-none"
+          style={{ background }}
+        />
+      </motion.div>
+      <motion.div
+        role="presentation"
+        className="absolute inset-0 -z-10 duration-500 group-hover:opacity-50 blur-2xl opacity-0 transition-opacity bg-black rounded-2xl scale-102"
+        style={{ rotateX: shadowX, rotateY: shadowY }}
+      />
+    </motion.div>
   );
 };
 
@@ -105,7 +182,7 @@ export default function Home() {
         }
       >
         <div className="sticky top-0 h-screen overflow-hidden">
-          <h1 className="text-4xl font-medium absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <h1 className="text-4xl font-medium absolute dark:text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             Works
           </h1>
           <motion.div
@@ -118,7 +195,7 @@ export default function Home() {
                 progress={mappedProgress}
                 index={index}
                 total={array.length}
-              />
+              ></Card>
             ))}
           </motion.div>
         </div>
