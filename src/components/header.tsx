1d9d2e3017b91cd391ff96c28c4bc4ed6480ddc3
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-07-01 21:49:28 +0800

    Header visibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useMotionValueEvent, useScroll } from "motion/react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const { scrollY } = useScroll();
+  const [backdrop, setBackdrop] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (y) => {
+    setBackdrop(y > 128);
+  });
   return (
-    <header className="fixed inset-x-0 top-0 z-50 flex justify-between gap-4 p-4">
+    <header className="fixed inset-x-0 top-0 z-50 flex justify-between gap-4 overflow-hidden p-4">
+      <div
+        className={cn(
+          "from-background pointer-events-none absolute inset-0 -z-10 h-[200%] bg-gradient-to-b to-transparent to-50% mask-b-from-0 mask-b-to-50% opacity-0 backdrop-blur-xl transition-opacity duration-1000",
+          backdrop && "opacity-100",
+        )}
+      ></div>
       <Link
         href="/"
         className="hover:text-main relative rounded font-medium text-zinc-800 transition focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:outline-none dark:text-zinc-100"
