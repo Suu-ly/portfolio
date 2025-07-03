04124443b41330fed2e95c0dbaa711ad4b97946e
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-07-03 20:16:04 +0800

    Remove unused stuff

--- src/components/page-template.tsx
+++ src/components/page-template.tsx
@@ -132,74 +132,72 @@ function PageTemplate({
   return (
     <main className="space-y-8 pt-24 sm:space-y-16 lg:space-y-32 lg:pt-32 dark:bg-zinc-950">
       <MaxWidthWrapper>
-        <ParallaxFade>
-          <div className="sm:mb-16 lg:mb-32">
-            <div className="animate-slide-in mb-4 flex items-center justify-between gap-4">
-              <span className="text-muted text-xs font-medium sm:text-base lg:text-lg">
-                {type}
-              </span>
-              <span className="text-muted rounded-full border border-zinc-600 px-4 py-1 text-xs font-medium sm:text-base lg:px-6 lg:py-1.5 lg:text-lg dark:border-zinc-400">
-                {date.getFullYear()}
-              </span>
-            </div>
-            <h1 className="font-display animate-slide-in animation-delay-75 text-main text-4xl font-bold sm:text-6xl lg:text-8xl">
-              {title}
-            </h1>
+        <div className="sm:mb-16 lg:mb-32">
+          <div className="animate-slide-in mb-4 flex items-center justify-between gap-4">
+            <span className="text-muted text-xs font-medium sm:text-base lg:text-lg">
+              {type}
+            </span>
+            <span className="text-muted rounded-full border border-zinc-600 px-4 py-1 text-xs font-medium sm:text-base lg:px-6 lg:py-1.5 lg:text-lg dark:border-zinc-400">
+              {date.getFullYear()}
+            </span>
           </div>
-          <div className="animate-slide-in animation-delay-150 grid grid-cols-2 gap-8 sm:gap-16">
-            <p className="text-muted col-span-2 self-center text-sm sm:col-span-1 sm:text-2xl">
-              {tagline}
+          <h1 className="font-display animate-slide-in animation-delay-75 text-main text-4xl font-bold sm:text-6xl lg:text-8xl">
+            {title}
+          </h1>
+        </div>
+        <div className="animate-slide-in animation-delay-150 grid grid-cols-2 gap-8 sm:gap-16">
+          <p className="text-muted col-span-2 self-center text-sm sm:col-span-1 sm:text-2xl">
+            {tagline}
+          </p>
+          <a
+            href={url}
+            target="_blank"
+            className="group hidden items-center gap-3 justify-self-start rounded-xl bg-zinc-900 p-4 pr-6 text-xl text-zinc-50 transition hover:bg-zinc-800 focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:outline-none sm:flex dark:bg-zinc-800"
+          >
+            <IconCornerDownRight className="size-5 transition-transform group-hover:translate-x-1" />
+            Visit Site
+          </a>
+          <div className="col-span-2 sm:col-span-1">
+            <p className="text-accent mb-2 text-sm font-medium sm:text-base">
+              About
             </p>
-            <a
-              href={url}
-              target="_blank"
-              className="group hidden items-center gap-3 justify-self-start rounded-xl bg-zinc-900 p-4 pr-6 text-xl text-zinc-50 transition hover:bg-zinc-800 focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:outline-none sm:flex dark:bg-zinc-800"
-            >
-              <IconCornerDownRight className="size-5 transition-transform group-hover:translate-x-1" />
-              Visit Site
-            </a>
-            <div className="col-span-2 sm:col-span-1">
-              <p className="text-accent mb-2 text-sm font-medium sm:text-base">
-                About
-              </p>
-              <p className="text-body sm:text-lg">{about}</p>
-            </div>
-            <div className="col-span-2 grid grid-cols-2 gap-8 sm:col-span-1 lg:gap-16">
-              <div className="sm:col-span-2 lg:col-span-1">
-                <p className="text-accent mb-2 text-sm font-medium sm:text-base">
-                  Role
-                </p>
-                <div className="flex flex-col">
-                  {roles.map((role) => (
-                    <p key={role} className="text-body sm:text-lg">
-                      {role}
-                    </p>
-                  ))}
-                </div>
-              </div>
-              <div className="sm:col-span-2 lg:col-span-1">
-                <p className="text-accent mb-2 text-sm font-medium sm:text-base">
-                  Tools
-                </p>
-                <div className="flex flex-col">
-                  {tools.map((tool) => (
-                    <p key={tool} className="text-body sm:text-lg">
-                      {tool}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <a
-              href={url}
-              target="_blank"
-              className="group col-span-2 inline-flex items-center gap-3 rounded-xl bg-zinc-900 px-6 py-5 text-zinc-50 transition hover:bg-zinc-800 focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:outline-none sm:hidden dark:bg-zinc-800"
-            >
-              <IconCornerDownRight className="size-5 transition-transform group-hover:translate-x-1" />
-              Visit Site
-            </a>
+            <p className="text-body sm:text-lg">{about}</p>
           </div>
-        </ParallaxFade>
+          <div className="col-span-2 grid grid-cols-2 gap-8 sm:col-span-1 lg:gap-16">
+            <div className="sm:col-span-2 lg:col-span-1">
+              <p className="text-accent mb-2 text-sm font-medium sm:text-base">
+                Role
+              </p>
+              <div className="flex flex-col">
+                {roles.map((role) => (
+                  <p key={role} className="text-body sm:text-lg">
+                    {role}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <div className="sm:col-span-2 lg:col-span-1">
+              <p className="text-accent mb-2 text-sm font-medium sm:text-base">
+                Tools
+              </p>
+              <div className="flex flex-col">
+                {tools.map((tool) => (
+                  <p key={tool} className="text-body sm:text-lg">
+                    {tool}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+          <a
+            href={url}
+            target="_blank"
+            className="group col-span-2 inline-flex items-center gap-3 rounded-xl bg-zinc-900 px-6 py-5 text-zinc-50 transition hover:bg-zinc-800 focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:outline-none sm:hidden dark:bg-zinc-800"
+          >
+            <IconCornerDownRight className="size-5 transition-transform group-hover:translate-x-1" />
+            Visit Site
+          </a>
+        </div>
       </MaxWidthWrapper>
       <div className="animate-slide-in animation-delay-225 relative mx-auto aspect-[4/3] w-full max-w-[1600px] overflow-hidden">
         <ImageFadeIn
