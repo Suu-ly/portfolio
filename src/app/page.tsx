e3d0a9a82fc8a9d5da3b28b3e27693ac2e75289e
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-08-08 22:56:45 +0800

    Fix homepage scrollbar issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default async function Home() {
 
   return (
     <div className="flex min-h-svh flex-col">
-      <main className="grow space-y-32 pt-32 pb-8 sm:pb-16 lg:py-32">
-        <MaxWidthWrapper className="flex w-full flex-col justify-center overflow-x-hidden p-4">
+      <main className="grow space-y-32 overflow-x-hidden pt-32 pb-8 sm:pb-16 lg:py-32">
+        <MaxWidthWrapper className="flex w-full flex-col justify-center p-4">
           <div className="animate-slide-in mb-6 self-start">
             <div className="animate-wiggle self-start">
               <HelloThere />
